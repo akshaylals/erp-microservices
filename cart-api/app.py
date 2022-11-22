141d4f5d7a89e94113e879e107f514002a3289d6
akshaylals <akshaylal.s@inapp.com>
2022-11-22 15:59:16 +0530

    cart API

--- cart-api/app.py
+++ cart-api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, request, Response
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -9,16 +9,71 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 
 db.init_app(app)
 
-class User(db.Model):
+class Cart(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     productId = db.Column(db.Integer, nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
 
+    def __init__(self, productId, quantity) -> None:
+        self.productId = productId
+        self.quantity = quantity
 
-@app.route("/")
-def index() -> str:
-    # transform a dict into an application/json response 
-    return jsonify({"message": "It Works"})
+
+
+@app.route("/cart", methods=['GET'])
+def get_all() -> str:
+    cart_items = []
+    for item in Cart.query.all():
+        cart_items.append({
+            "id": item.id,
+            "productId": item.productId,
+            "quantity": item.quantity
+        })
+    return jsonify({"cart": cart_items})
+
+
+@app.route("/cart/<int:id>", methods=['GET'])
+def get_one(id) -> str:
+    item = Cart.query.filter_by(id=id).first()
+    if item:
+        return jsonify({"cartitem": {
+            "id": item.id,
+            "productId": item.productId,
+            "quantity": item.quantity
+        }})
+    return jsonify({"message": "Not found" }), 404
+    
+
+@app.route("/cart", methods=['POST'])
+def post() -> str:
+    if not request.json:
+        return jsonify({"message": "bad request"}), 400
+
+    productId = request.json['productId']
+    quantity = request.json['quantity']
+
+    try:
+        db.session.add(Cart(productId, quantity))
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"message": "bad request"}), 400
+    else:
+        return jsonify({"message": "success" }), 201
+
+
+@app.route("/cart/<int:id>", methods=['DELETE'])
+def delete(id) -> str:
+    try:
+        item = Cart.query.filter_by(id=id)
+    except:
+        return jsonify({"message": "Internal Server Error" }), 500
+    else:
+        if item.delete() == 1:
+            db.session.commit()
+            return jsonify({}), 204
+        else:
+            return jsonify({"message": "Not found" }), 404
+
         
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080) 
