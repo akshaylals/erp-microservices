b511f8e0038b1e17ede1514cde431daa13da7dc7
Anupam <kidab2k@gmail.com>
2022-12-01 15:10:17 +0530

    Added Token Validation - Products API

--- products-api/routes/products.js
+++ products-api/routes/products.js
@@ -1,17 +1,23 @@
 const express = require('express')
 const { getProducts, getProduct, getSearchProduct, getSearchProductFiltered } = require('../controllers/productController')
 const router = express.Router()
+const { auth } = require('express-oauth2-jwt-bearer');
+
+const checkJwt = auth({
+    audience: 'https://dev-1ipheitoccjnh67e.us.auth0.com/api/v2/',
+    issuerBaseURL: `https://dev-1ipheitoccjnh67e.us.auth0.com/`,
+});
 
 // GET all products
-router.get('/', getProducts)
+router.get('/', checkJwt, getProducts)
 
 // GET a single product
-router.get('/:id', getProduct)
+router.get('/:id', checkJwt, getProduct)
 
 // GET searched product
-router.get('/search/:search', getSearchProduct)
+router.get('/search/:search', checkJwt, getSearchProduct)
 
 // GET searched and filtered product
-router.get('/search/:search/:filter', getSearchProductFiltered)
+router.get('/search/:search/:filter', checkJwt, getSearchProductFiltered)
 
 module.exports = router
